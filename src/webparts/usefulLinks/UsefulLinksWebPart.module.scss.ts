cb344013a4787cf0a9e001ff8a7a54edcd52fe64
Jason Clark <jason_mc@btinternet.com>
2020-03-13 00:18:53 +0000

    updated src files

---
+++ src/webparts/usefulLinks/UsefulLinksWebPart.module.scss.ts
@@ -0,0 +1,19 @@
+/* tslint:disable */
+require("./UsefulLinksWebPart.module.css");
+const styles = {
+  usefulLinks: 'usefulLinks_2b334c39',
+  container: 'container_2b334c39',
+  row: 'row_2b334c39',
+  column: 'column_2b334c39',
+  'ms-Grid': 'ms-Grid_2b334c39',
+  title: 'title_2b334c39',
+  subTitle: 'subTitle_2b334c39',
+  description: 'description_2b334c39',
+  button: 'button_2b334c39',
+  label: 'label_2b334c39',
+  list: 'list_2b334c39',
+  listItem: 'listItem_2b334c39'
+};
+
+export default styles;
+/* tslint:enable */
